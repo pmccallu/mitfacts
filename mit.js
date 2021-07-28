0c80873a43ebe508b5c78c226cf548a422f07b9c
Peter McCallum <petermccallum@PJM-MacBook-Pro.local>
2021-07-28 17:01:39 -0500

    fixed date

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['pink', 'red'],
-    mascot: "robot",
-    founded: 1900,
-    motto: 'Carpe diem',
+    colors: ['Silver Gray'],
+    mascot: "mens",
+    founded: 1861,
+    motto: 'Mens',
 };
